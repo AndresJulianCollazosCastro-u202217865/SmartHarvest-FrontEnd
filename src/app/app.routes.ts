4557555125e170906b5b25cff58728c5dc834322
Collax-ops <andresjuliancollazoscastro@gmail.com>
2025-06-02 22:15:25 -0500

    Proyecto inicial Angular + .gitignore WebStorm configurado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {Dashboard} from './SmartHarvest/dashboard/dashboard';
+import {Cultivos} from './SmartHarvest/cultivos/cultivos';
+import {Alertas} from './SmartHarvest/alertas/alertas';
+import {Recommendations} from './SmartHarvest/recommendations/recommendations';
+import {LearningResources} from './SmartHarvest/learning-resources/learning-resources';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'SmartHarvest/dashboard', component: Dashboard },
+  { path: 'SmartHarvest/cultivos', component: Cultivos},
+  { path: 'SmartHarvest/alertas', component: Alertas },
+  { path: 'SmartHarvest/recommendations', component: Recommendations },
+  { path: 'SmartHarvest/learning-resources', component: LearningResources },
+  { path: '**', redirectTo: 'SmartHarvest/dashboard' }
+];
