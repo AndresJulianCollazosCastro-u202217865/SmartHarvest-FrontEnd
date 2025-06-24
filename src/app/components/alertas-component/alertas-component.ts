19c7e9e11793e4380d8654bb199aa7cc3f098d43
Collax-ops <andresjuliancollazoscastro@gmail.com>
2025-06-23 20:49:20 -0500

    feat(alertas): recarga dinámica y filtrado por tipo

--- src/app/components/alertas-component/alertas-component.ts
+++ src/app/components/alertas-component/alertas-component.ts
@@ -1,8 +1,11 @@
 import {Component, inject} from '@angular/core';
 import {MatFormField, MatLabel} from '@angular/material/input';
 import {MatOption, MatSelect} from '@angular/material/select';
-import {WeatherAlert, WeatherAlertType} from '../../model/weater-alert';
+import {WeatherAlert, WeatherAlertEstate, WeatherAlertType} from '../../model/weater-alert';
 import {WeatherAlertService} from '../../services/weather-alert-service';
+import {MatCard} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-alertas-component',
@@ -11,6 +14,9 @@ import {WeatherAlertService} from '../../services/weather-alert-service';
     MatSelect,
     MatOption,
     MatLabel,
+    MatCard,
+    MatButton,
+    FormsModule,
   ],
   templateUrl: './alertas-component.html',
   styleUrl: './alertas-component.css'
@@ -18,18 +24,44 @@ import {WeatherAlertService} from '../../services/weather-alert-service';
 export class AlertasComponent {
   alertas: WeatherAlert[] = [];
   selectedTipo: WeatherAlertType | '' = '';
+  selectedEstado: WeatherAlertEstate
+  private userId!: number
 
   weatherAlertService = inject(WeatherAlertService);
 
   ngOnInit(): void {
-    this.weatherAlertService.getAlerts().subscribe((data) => {
-      console.log(data);
-      this.alertas = data;
-    });
+    this.userId = Number(localStorage.getItem('userId'));
+    this.onTipoChange(this.selectedTipo);
   }
 
   onTipoChange(tipo: WeatherAlertType | ''): void {
     this.selectedTipo = tipo;
+    const typeParam = this.selectedTipo || undefined;
+    console.log('Filtrando alertas con:', {
+      userId: this.userId,
+      type: typeParam,
+      status: this.selectedEstado || undefined
+    });
+    this.weatherAlertService
+      .getAlertsFiltered(this.userId, typeParam, this.selectedEstado || undefined)
+      .subscribe({
+        next: alerts => this.alertas = alerts,
+        error: err    => console.error('Error al filtrar alertas', err)
+      });
   }
 
+  marcarComoLeida(alerta: WeatherAlert) {
+    this.weatherAlertService.updateAlertState(this.userId,alerta.id).subscribe({
+      next: () => {
+          alerta.estado = WeatherAlertEstate.LEIDA
+      },
+      error: err => console.error('Error marcando alerta como leída', err)
+    });
+  }
+
+  trackById(_: number, alerta: WeatherAlert) {
+    return alerta.id;
+  }
+
+  protected readonly WeatherAlertType = WeatherAlertType;
 }
--- src/app/services/weather-alert-service.ts
+++ src/app/services/weather-alert-service.ts
@@ -1,9 +1,9 @@
 import {inject, Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {map, Observable} from 'rxjs';
 import {AlertReport} from '../model/alert-report';
-import {WeatherAlert} from '../model/weater-alert';
+import {WeatherAlert, WeatherAlertEstate, WeatherAlertType} from '../model/weater-alert';
 
 @Injectable({
   providedIn: 'root'
@@ -25,4 +25,23 @@ export class WeatherAlertService {
     )
   }
 
+  getAlertsFiltered(
+    userId: number,
+    type?: WeatherAlertType,
+    status?: WeatherAlertEstate
+  ): Observable<WeatherAlert[]> {
+    let params = new HttpParams().set('userId', userId.toString());
+    if (type)   params = params.set('type', type);
+    if (status) params = params.set('status', status);
+    console.log('Params en servicio:', params.toString());
+    return this.http.get<WeatherAlert[]>(
+      `${this.url}/SmartHarvest/alertas/filter`,
+      { params }
+    );
+  }
+
+  updateAlertState(userId: number, alertId: number): Observable<number> {
+    const url = `${this.url}/SmartHarvest/alertas/mark-as-read/${userId}/${alertId}`;
+    return this.http.put<number>(url, null);
+  }
 }
